bae12a3efc48598b45f51be6f520273f09a096a4
Eric Lamb <eric@mithra62.com>
2015-09-26 19:37:44 -0700

    Adds spatial columns

---
+++ BackupPro/Backup/Database/Engines/Php/Columns/Linestring.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * mithra62 - Backup Pro
+ *
+ * @author		Eric Lamb <eric@mithra62.com>
+ * @copyright	Copyright (c) 2015, mithra62, Eric Lamb.
+ * @link		http://mithra62.com/
+ * @version		3.0
+ * @filesource 	./mithra62/BackupPro/Backup/Database/Php/Columns/Linestring.php
+ */
+ 
+namespace mithra62\BackupPro\Backup\Database\Engines\Php\Columns;
+
+use mithra62\BackupPro\Backup\Database\Engines\Php\Columns\Point;
+
+/**
+ * Backup Pro - Geometry Column Object
+ *
+ * Handles processing data from a MySQL Linestring column
+ *
+ * @package 	Backup\Database\Engines\Php
+ * @author		Eric Lamb <eric@mithra62.com>
+ */
+class Linestring extends Point
+{
+
+}
---
+++ BackupPro/Backup/Database/Engines/Php/Columns/Multipolygon.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * mithra62 - Backup Pro
+ *
+ * @author		Eric Lamb <eric@mithra62.com>
+ * @copyright	Copyright (c) 2015, mithra62, Eric Lamb.
+ * @link		http://mithra62.com/
+ * @version		3.0
+ * @filesource 	./mithra62/BackupPro/Backup/Database/Php/Columns/Multipolygon.php
+ */
+ 
+namespace mithra62\BackupPro\Backup\Database\Engines\Php\Columns;
+
+use mithra62\BackupPro\Backup\Database\Engines\Php\Columns\Point;
+
+/**
+ * Backup Pro - Multipolygon Column Object
+ *
+ * Handles processing data from a MySQL Multipolygon column
+ *
+ * @package 	Backup\Database\Engines\Php
+ * @author		Eric Lamb <eric@mithra62.com>
+ */
+class Multipolygon extends Point
+{
+
+}
