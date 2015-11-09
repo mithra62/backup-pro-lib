cd08cbf53263ee8cdf62dd17cd1e13b276687d4f
Eric Lamb <eric@mithra62.com>
2015-11-09 15:24:49 -0800

    Updates SFTP language copy

--- BackupPro/language/sftp_storage.php
+++ BackupPro/language/sftp_storage.php
@@ -16,20 +16,21 @@
  */
  
 $lang = array(
-    'backup_pro_sftp_storage_driver_name' => 'SFTP/ SSH Storage',
+    'backup_pro_sftp_storage_driver_name' => 'SFTP Storage',
     'backup_pro_sftp_storage_driver_description' => 'This Driver will store backups on a remote SFTP/SSH server.',
     'sftp_host' => 'SFTP Hostname',
     'sftp_host_instructions' => 'The address or domain to the remote server. Don\'t include any prefix like http:// or ftp://',
-    'sftp_username' => 'SFTP / SSH  Username',
+    'sftp_username' => 'SFTP Username',
     'sftp_username_instructions' => 'If you don\'t know what this is there\'s a good chance you\'ll have to talk to your host to get FTP sync up and running. ',
     'sftp_password' => 'SFTP / SSH Password',
     'sftp_password_instructions' => 'The password is encrypted for security before storage.',
     'sftp_port' => 'SFTP / SSH Port',
-    'sftp_port_instructions' => 'The default is 22 but if your host uses a differnt port for SFTP / SSH update it here.',
+    'sftp_port_instructions' => 'The default is 22 but if your host uses a differnt port for SFTP update it here.',
     'sftp_timeout' => 'Connection Timeout',
     'sftp_timeout_instructions' => 'The maximum time to use for all network operations. Be aware that this value may require tweaking your php.ini settings for the script timeout.',
-    'sftp_root' => 'SFTP / SSH Store Location',
+    'sftp_store_location' => 'SFTP Store Location',
     'sftp_root_instructions' => 'Where on the remote server do you want to store the files. This directory has to exist before the settings can be saved.',
+    'sftp_root' => 'SFTP Store Location',
     'sftp_private_key' => 'Private Key Path',
     'sftp_private_key_instructions' => 'The full system path to where your private key file is stored if you\'d like to use that for authentication instead of username and password. ',
 );
