7b38ccccc0055163beb0b3948a1c727b551c0201
Eric Lamb <eric@mithra62.com>
2015-11-10 14:33:54 -0800

    Updates language copy for SFTP and Dropbox settings forms

--- BackupPro/language/dropbox_storage.php
+++ BackupPro/language/dropbox_storage.php
@@ -19,7 +19,9 @@ $lang = array(
     'backup_pro_dropbox_storage_driver_name' => 'Dropbox Storage',
     'backup_pro_dropbox_storage_driver_description' => 'This Driver will store backups on Dropbox.',
     'dropbox_access_token' => 'Oauth Access Token',
-    'dropbox_access_token_instructions' => '',
+    'dropbox_access_token_instructions' => 'Dropbox allows you to manually generate your own Oauth Access Token in the <a href="https://www.dropbox.com/developers/apps" target="_blank">Dropbox App Console</a>. You can get one there.',
     'dropbox_app_secret' => 'App Secret',
+    'dropbox_app_secret_instructions' => 'When you create your Dropbox App, you\'ll receive an App Secret. Enter that so Dropbox knows where to put your files. ',
     'dropbox_prefix' => 'Prefix',
+    'dropbox_prefix_instructions' => 'Basically, what folder do you want to store your backups in?',
 );
