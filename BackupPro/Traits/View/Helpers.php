a0744bedcbaa4fe20b2e7c719482ad08ce4c653f
Eric Lamb <eric@mithra62.com>
2016-01-30 11:48:00 -0800

    Adds region dropdown options

--- BackupPro/Traits/View/Helpers.php
+++ BackupPro/Traits/View/Helpers.php
@@ -16,6 +16,18 @@ trait Helpers
         'db_restore_methods' => array(
             'php' => 'php',
             'mysql' => 'mysql'
+        ),
+        's3_regions' => array(
+            '0' => 'US Standard',
+            'us-west-2' => 'Oregon',
+            'us-west-1' => 'Northern California',
+            'eu-west-1' => 'Ireland',
+            'ap-southeast-1' => 'Singapore',
+            'ap-northeast-1' => 'Tokyo',
+            'ap-southeast-2' => 'Sydney',
+            'sa-east-1' => 'Sao Paulo',
+            'eu-central-1' => 'Frankfurt',
+            'ap-northeast-2' => 'Seoul'
         )
     );
     
