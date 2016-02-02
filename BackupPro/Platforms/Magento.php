77533e8769bb04bb2e89d3aadf485cc72e212290
Eric Lamb <eric@mithra62.com>
2016-02-02 09:03:34 -0800

    Adds base Magento Platform objects

---
+++ BackupPro/Platforms/Magento.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * mithra62 - Backup Pro
+ *
+ * @author		Eric Lamb <eric@mithra62.com>
+ * @copyright	Copyright (c) 2015, mithra62, Eric Lamb.
+ * @link		http://mithra62.com/
+ * @version		3.0
+ * @filesource 	./mithra62/BackupPro/Platforms/Magento.php
+ */
+namespace mithra62\BackupPro\Platforms;
+
+use mithra62\Platforms\Magento as m62Mag;
+use mithra62\BackupPro\Platforms\PlatformInterface;
+
+/**
+ * Backup Pro - Magento Bridge
+ *
+ * Contains the Magento specific items Backup Pro needs
+ *
+ * @package mithra62\BackupPro
+ * @author Eric Lamb <eric@mithra62.com>
+ */
+class Magento extends m62Mag implements PlatformInterface
+{
+    
+}
---
+++ BackupPro/Platforms/View/Magento.php
@@ -0,0 +1,10 @@
+<?php
+namespace mithra62\BackupPro\Platforms\View;
+
+use mithra62\Platforms\View\Magento AS Ext;
+use mithra62\BackupPro\Traits\View\Helpers As ViewHelpers;
+
+class Magento extends Ext
+{
+    use ViewHelpers;
+}
