525762b78975609378ca5b2edfd0a3883a2e0929
Eric Lamb <eric@mithra62.com>
2016-03-11 15:01:10 -0800

    Replaces header calls with native function

--- BackupPro/Platforms/Controllers/Rest.php
+++ BackupPro/Platforms/Controllers/Rest.php
@@ -139,18 +139,7 @@ class Rest implements Routable, \mithra62\BackupPro\BackupPro
      */
     public function getRequestHeaders() 
     {
-        $headers = array();
-        foreach($_SERVER as $key => $value) 
-        {
-            if (substr($key, 0, 5) <> 'HTTP_') 
-            {
-                continue;
-            }
-            
-            $header = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
-            $headers[$header] = $value;
-        }
-        return $headers;
+        return \getallheaders();
     }
     
     /**
