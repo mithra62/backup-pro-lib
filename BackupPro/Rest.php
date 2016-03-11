7ab0407106b699f6faea7475e9abd8a59a5682fb
Eric Lamb <eric@mithra62.com>
2016-03-11 12:27:07 -0800

    Moves objects to base mithra62 library and sets up abstraction

--- BackupPro/Rest.php
+++ BackupPro/Rest.php
@@ -1,12 +1,10 @@
 <?php
 namespace mithra62\BackupPro;
 
-class Rest
+use mithra62\Rest AS m62Rest;
+
+class Rest extends m62Rest
 {
-    protected $lang = null;
-    protected $platform = null;
-    protected $server = null;
-    
     public function getServer()
     {
         if(is_null($this->server))
@@ -14,19 +12,7 @@ class Rest
             $this->server = new Rest\Server($this->platform, $this);
             //$this->server
         }
-        
+    
         return $this->server;
-    }
-    
-    public function setPlatform(\mithra62\Platforms\AbstractPlatform $platform)
-    {
-        $this->platform = $platform;
-        return $this;
-    }
-    
-    public function setLang(\mithra62\Language $lang)
-    {
-        $this->lang = $lang;
-        return $this;
     }
 }
--- BackupPro/Rest/Server.php
+++ BackupPro/Rest/Server.php
@@ -1,54 +1,11 @@
 <?php
-/**
- * mithra62 - Backup Pro
- *
- * @copyright	Copyright (c) 2015, mithra62, Eric Lamb.
- * @link		http://mithra62.com/
- * @version		3.0
- * @filesource 	./mithra62/BackupPro/Rest/Server.php
- */
- 
 namespace mithra62\BackupPro\Rest;
 
+use mithra62\Rest\AbstractServer;
 use Respect\Rest\Router;
 
-/**
- * Backup Pro - REST Server
- *
- * Sets up and fires off the Backup Pro REST Server
- *
- * @package mithra62\BackupPro
- * @author Eric Lamb <eric@mithra62.com>
- */
-class Server
+class Server extends AbstractServer
 {
-    /**
-     * The Platform object
-     * @var \mithra62\Platforms\AbstractPlatform
-     */
-    protected $platform = null;
-    
-    /**
-     * The REST object
-     * @var \mithra62\BackupPro\Rest
-     */
-    protected $rest = null;
-    
-    /**
-     * Set it up
-     * @param \mithra62\Platforms\AbstractPlatform $platform
-     * @param \mithra62\BackupPro\Rest $rest
-     */
-    public function __construct(\mithra62\Platforms\AbstractPlatform $platform, \mithra62\BackupPro\Rest $rest)
-    {
-        $this->platform = $platform;
-        $this->rest = $rest;
-    }
-    
-    /**
-     * Outlines the Server routes
-     * @return void
-     */
     public function run()
     {
         $r3 = new Router('/backup_pro/api');
