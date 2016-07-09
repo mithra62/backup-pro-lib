d856d4bc131e1a487e848b6afafb1940fe02bf31
Eric Lamb <eric@mithra62.com>
2016-07-09 14:08:36 -0700

    adds bad files to ignore

--- mithra62/BackupPro/tests/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Initializes the environment for unit testing
- */
-
-/**
- * Setup the Composer details
- */
-require_once realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
