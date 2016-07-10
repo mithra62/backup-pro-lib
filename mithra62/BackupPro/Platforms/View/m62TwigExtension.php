53976683478677bf49aeee6ebf67ff072380345a
Eric Lamb <eric@mithra62.com>
2016-07-10 14:27:41 -0700

    Updates namespace

--- mithra62/BackupPro/Platforms/View/m62TwigExtension.php
+++ mithra62/BackupPro/Platforms/View/m62TwigExtension.php
@@ -1,7 +1,7 @@
 <?php
 namespace mithra62\BackupPro\Platforms\View;
 
-use mithra62\Platforms\View\m62TwigExtension AS Ext;
+use JaegerApp\Platforms\View\m62TwigExtension AS Ext;
 use mithra62\BackupPro\Traits\View\Helpers As ViewHelpers;
 
 class m62TwigExtension extends Ext
