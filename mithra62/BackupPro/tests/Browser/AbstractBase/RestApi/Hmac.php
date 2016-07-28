32150c4655f9b67baa33dbba02a3990511a4d665
Eric Lamb <eric@mithra62.com>
2016-07-28 12:57:31 -0700

    adds tests

--- mithra62/BackupPro/tests/Browser/AbstractBase/RestApi/Hmac.php
+++ mithra62/BackupPro/tests/Browser/AbstractBase/RestApi/Hmac.php
@@ -66,12 +66,50 @@ abstract class Hmac extends TestFixture
         $client = new BpApiClient($this->rest_client_details);
         
         $data = $client->get('/info/site');
-        print_r($data);
-        exit;
+        $this->assertInstanceOf('\JaegerApp\Rest\Client\ApiProblem', $data);
+        $this->assertEquals(404, $data->getStatus());
+        $this->assertEquals('Not Found', $data->getTitle());
+    }
+    
+    /**
+     * @depends test404OnNotEnabled
+     */    
+    public function test403NotAuthorized()
+    {
+        $this->setGoodRestApi();
+        $this->session->visit($this->url('settings_api'));
+        $page = $this->session->getPage();
         
+        $url = $page->findById('rest_api_url_wrap');
+        $api_url = $url->getAttribute('href');
+        
+        $this->rest_client_details = array(
+            'site_url' => $api_url,
+            'api_key' => 'fdsa',
+            'api_secret' => 'fdsa'
+        );
+        
+        $client = new BpApiClient($this->rest_client_details);
+        
+        $data = $client->get('/info/site');
+        $this->assertInstanceOf('JaegerApp\Rest\Client\ApiProblem', $data);
+        $this->assertEquals(403, $data->getStatus());
+        $this->assertEquals('Unauthorized', $data->getTitle());
+    }
+    
+    /**
+     * @depends test403NotAuthorized
+     */    
+    public function testAuthorized()
+    {
+        $this->setGoodRestApi();
+        $this->setupRestApiClientCreds();
+        
+        $client = new BpApiClient($this->rest_client_details);
+        $data = $client->get('/info/site');
+        $this->assertInstanceOf('JaegerApp\Rest\Client\Hal', $data);
+        $this->assertEquals('/info/site', $data->getUri());
         $this->uninstall_addon();
-        exit;
-        
     }
     
 }
