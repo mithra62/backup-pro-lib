b6b3d1acb9f7c90ca875b116d2d6972083e0ea5f
Eric Lamb <eric@mithra62.com>
2016-08-08 01:50:51 -0700

    adds basic object

--- mithra62/BackupPro/Platforms/Controllers/Drupal8.php
+++ mithra62/BackupPro/Platforms/Controllers/Drupal8.php
@@ -1,1 +1,81 @@
 <?php
+/**
+ * mithra62 - Backup Pro
+ *
+ * @copyright	Copyright (c) 2015, mithra62, Eric Lamb.
+ * @link		http://mithra62.com/
+ * @version		3.0
+ * @filesource 	./mithra62/BackupPro/Controllers/Drupal8.php
+ */
+namespace mithra62\BackupPro\Platforms\Controllers;
+
+use Drupal\Core\Controller\ControllerBase;
+use mithra62\BackupPro\Platforms\Drupal8 as Platform;
+use mithra62\BackupPro\Traits\Controller;
+use mithra62\BackupPro\Platforms\View\Smarty;
+
+/**
+ * Backup Pro - Drupal 8 Base Controller
+ *
+ * Starts the Controllers up
+ *
+ * @package BackupPro\Controllers
+ * @author Eric Lamb <eric@mithra62.com>
+ */
+class Drupal8 extends ControllerBase
+{
+    use Controller;
+
+    /**
+     * The abstracted platform object
+     * 
+     * @var \mithra62\Platforms\Craft
+     */
+    protected $platform = null;
+
+    /**
+     * The Backup Pro Settings
+     * 
+     * @var array
+     */
+    protected $settings = array();
+
+    /**
+     * A container of system messages and errors
+     * 
+     * @var array
+     */
+    protected $bp_errors = array();
+
+    /**
+     * Set it up
+     * 
+     * @param unknown $id            
+     * @param string $module            
+     */
+    public function __construct()
+    {
+        $this->initController();
+        $this->platform = new Platform();
+        $this->m62->setService('platform', function ($c) {
+            return $this->platform;
+        });
+        
+        $this->m62->setDbConfig($this->platform->getDbCredentials());
+        $this->settings = $this->services['settings']->get();
+        $errors = $this->services['errors']->checkWorkingDirectory($this->settings['working_directory'])
+            ->checkStorageLocations($this->settings['storage_details']);
+            //->licenseCheck($this->settings['license_number'], $this->services['license']);
+        
+        if ($errors->totalErrors() == '0') {
+            $errors = $errors->checkBackupState($this->services['backups'], $this->settings);
+        }
+        
+        $this->bp_errors = $errors->getErrors();
+        
+        $this->view_helper = new Smarty($this->services['lang'], $this->services['files'], $this->services['settings'], $this->services['encrypt'], $this->platform);
+        $this->m62->setService('view_helpers', function ($c) {
+            return $this->view_helper;
+        });
+    }
+}
